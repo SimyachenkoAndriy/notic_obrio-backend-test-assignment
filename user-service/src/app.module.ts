d2d892118af4ef28b965ebd154b7cb836490e094
Andriy Simyachenko <simyachenko.andriy@gmail.com>
2025-03-31 00:48:32 +0300

    environment: setting up service creation

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,6 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, Controller, Get } from '@nestjs/common';
+
+@Controller()
+class AppController {
+  @Get()
+  getHello(): string {
+    return 'User-service dock container was successfully built and started!';
+  }
+}
 
 @Module({
   imports: [],
+  controllers: [AppController],
 })
 export class AppModule {}
